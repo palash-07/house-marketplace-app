b9342d886fd9a200b0ac60fcdf76ade6aa085102
palashg <palashguptakatni@gmail.com>
2022-01-27 19:15:57 +0530

    Listing Item details(Listing.jsx) -> Contact owner(Contact.jsx) updated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -27,6 +29,8 @@ function App() {
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />       
         </Routes>
         <Navbar />
       </Router>
