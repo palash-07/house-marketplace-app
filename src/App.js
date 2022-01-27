6624679707335fc05f4a3e7e5955b4ef07e189c6
palashg <palashguptakatni@gmail.com>
2022-01-27 15:43:57 +0530

    Create-listing form added (all updations to firestore and storage completed)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Offers from './pages/Offers'
 import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
+import CreateListing from './pages/CreateListing'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path='/sign-in' element={<Signin />} />
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
